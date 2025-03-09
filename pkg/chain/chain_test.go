bdd5e548072c3f3b74f238c0e1468a45913caf98
maxwu <maxwunj@gmail.com>
2025-03-09 18:16:53 +1100

    Add workflow to register new version to go.dev

--- pkg/chain/chain_test.go
+++ pkg/chain/chain_test.go
@@ -7,84 +7,105 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	_ "github.com/maxwu/gotest-labels/apply"
 )
 
-func TestChain(t *testing.T) {
+// @env=local
+func TestNewChain(t *testing.T) {
 	t.Parallel()
 
-	t.Run("Verify the Chain without HTTP middleware function is empty", func(t *testing.T) {
+	t.Run("Create an empty Chain", func(t *testing.T) {
 		t.Parallel()
 		chain := NewChain()
 		assert.Equal(t, 0, len(chain.Links))
 	})
 
-	t.Run("Verify the Chain stops processing on failed Link", func(t *testing.T) {
+	t.Run("Create a Chain with links", func(t *testing.T) {
 		t.Parallel()
-		processed := []string{}
 		linkA := func(h http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				processed = append(processed, "linkA")
-				h.ServeHTTP(w, r)
-			})
+			return h
 		}
-
 		linkB := func(h http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-				processed = append(processed, "linkB")
-				http.Error(w, "linkB generated an error", http.StatusInternalServerError)
-			})
+			return h
 		}
-		linkC := func(_ http.Handler) http.Handler {
-			return http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
-				processed = append(processed, "linkC")
-			})
-		}
-
-		h := NewChain(linkA, linkB, linkC).GetHandler()
-		req, _ := http.NewRequest("GET", "/path", nil)
-		rr := httptest.NewRecorder()
-
-		// handle an incoming request
-		h.ServeHTTP(rr, req)
-
-		assert.Equal(t, []string{"linkA", "linkB"}, processed)
-		assert.Equal(t, http.StatusInternalServerError, rr.Result().StatusCode)
-		body, _ := io.ReadAll(rr.Result().Body)
-		assert.Contains(t, string(body), "linkB generated an error")
-	})
-
-	t.Run("Sunnyday scenario", func(t *testing.T) {
-		t.Parallel()
-		processed := []string{}
-		linkA := func(h http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				processed = append(processed, "linkA")
-				h.ServeHTTP(w, r)
-			})
-		}
-
-		linkB := func(h http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				processed = append(processed, "linkB")
-				h.ServeHTTP(w, r)
-			})
-		}
-
 		linkC := func(h http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				processed = append(processed, "linkC")
-				w.WriteHeader(http.StatusOK)
-				h.ServeHTTP(w, r)
-			})
+			return h
 		}
-
-		h := NewChain(linkA, linkB, linkC).GetHandler()
-		req, _ := http.NewRequest("POST", "/doesnt-matter", nil)
-		rr := httptest.NewRecorder()
-
-		h.ServeHTTP(rr, req)
-
-		assert.Equal(t, []string{"linkA", "linkB", "linkC"}, processed)
-		assert.Equal(t, http.StatusOK, rr.Result().StatusCode)
+		chain := NewChain(linkA, linkB, linkC)
+		assert.Equal(t, 3, len(chain.Links))
+		assert.Equal(t, []LinkFunc{linkA, linkB, linkC}, chain.Links)
 	})
+}
+
+// @group=unittest
+// @env=ci
+func TestGetHandlerWithFailingLink(t *testing.T) {
+	t.Parallel()
+	processed := []string{}
+	linkA := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			processed = append(processed, "linkA")
+			h.ServeHTTP(w, r)
+		})
+	}
+
+	linkB := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			processed = append(processed, "linkB")
+			http.Error(w, "linkB generated an error", http.StatusInternalServerError)
+		})
+	}
+	linkC := func(_ http.Handler) http.Handler {
+		return http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+			processed = append(processed, "linkC")
+		})
+	}
+
+	h := NewChain(linkA, linkB, linkC).GetHandler()
+	req, _ := http.NewRequest("GET", "/path", nil)
+	rr := httptest.NewRecorder()
+
+	// handle an incoming request
+	h.ServeHTTP(rr, req)
+
+	assert.Equal(t, []string{"linkA", "linkB"}, processed)
+	assert.Equal(t, http.StatusInternalServerError, rr.Result().StatusCode)
+	body, _ := io.ReadAll(rr.Result().Body)
+	assert.Contains(t, string(body), "linkB generated an error")
+}
+
+// @env=ci
+func TestGetHandlerSunnyDay(t *testing.T) {
+	t.Parallel()
+	processed := []string{}
+	linkA := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			processed = append(processed, "linkA")
+			h.ServeHTTP(w, r)
+		})
+	}
+
+	linkB := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			processed = append(processed, "linkB")
+			h.ServeHTTP(w, r)
+		})
+	}
+
+	linkC := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			processed = append(processed, "linkC")
+			w.WriteHeader(http.StatusOK)
+			h.ServeHTTP(w, r)
+		})
+	}
+
+	h := NewChain(linkA, linkB, linkC).GetHandler()
+	req, _ := http.NewRequest("POST", "/doesnt-matter", nil)
+	rr := httptest.NewRecorder()
+
+	h.ServeHTTP(rr, req)
+
+	assert.Equal(t, []string{"linkA", "linkB", "linkC"}, processed)
+	assert.Equal(t, http.StatusOK, rr.Result().StatusCode)
 }
